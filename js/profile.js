101b41c7b890d48f91976466b5e8cd1c9f9e6d80
Olalekan Ayodele <longyarnz@gmail.com>
2019-03-07 11:09:30 +0100

    Fetch the profile of the user after sign up/log in

--- js/profile.js
+++ js/profile.js
@@ -1,3 +1,52 @@
+function createAddMoreInput() {
+  const form = document.querySelector('form');
+  const label = document.querySelector('label[for="profile"]').cloneNode();
+  const fileInput = document.querySelector('input[type="file"]').cloneNode();
+
+  label.htmlFor = 'add-more';
+  label.textContent = 'Add More Images To Attract Clients';
+
+  fileInput.id = 'add-more';
+  fileInput.name = 'more';
+  fileInput.multiple = true;
+  fileInput.placeholder = 'Select as many as you want';
+
+  form.insertBefore(label, form.children.finalSubmit);
+  form.insertBefore(fileInput, form.children.finalSubmit);
+
+}
+
+function createGalleryForWorker() {
+  const footer = document.querySelector('footer');
+  const section = document.createElement('section');
+  section.classList.add('container');
+
+  const div = document.createElement('div');
+  div.classList.add('show-worker-images');
+
+  const h3 = document.createElement('h3');
+  h3.textContent = `MORE IMAGES OF ${profile.username.toUpperCase()}`;
+
+  const gallery = document.createElement('div');
+  gallery.classList.add('gallery');
+
+  const tile = src => {
+    const div = document.createElement('div');
+    const img = document.createElement('img');
+    img.src = `https://images.pinkettu.com.ng/${src}`;
+    const button = document.createElement('button');
+    button.textContent = 'DELETE';
+    div.append(img, button);
+    return div;
+  }
+
+  const tiles = profile.images.slice(1).map(src => tile(src));
+  gallery.append(...tiles);
+  div.append(h3, gallery);
+  section.appendChild(div);
+  document.insertBefore(section, footer);
+}
+
 async function fetchUserProfile() {
   const appIsLive = location.hostname !== '127.0.0.1';
   const API = appIsLive ? 'https://api.pinkettu.com.ng' : 'http://127.0.0.1:3001';
@@ -13,43 +62,15 @@ async function fetchUserProfile() {
     location.href = '/signup.html';
   }
   else {
-    console.log(profile);
     const form = document.querySelector('form');
     const img = document.querySelector('.pic-wrapper img');
     img.src = `https://images.pinkettu.com.ng/${profile.images[0]}`;
     form[0].value = profile.username;
     form[1].value = profile.worker;
-    
-    if (profile.images.length > 1) {
-      const footer = document.querySelector('footer');
-      const section = document.createElement('section');
-      section.classList.add('container');
 
-      const div = document.createElement('div');
-      div.classList.add('show-worker-images');
+    if (profile.worker) createAddMoreInput();
 
-      const h3 = document.createElement('h3');
-      h3.textContent = `MORE IMAGES OF ${profile.username.toUpperCase()}`;
-
-      const gallery = document.createElement('div');
-      gallery.classList.add('gallery');
-
-      const tile = src => {
-        const div = document.createElement('div');
-        const img = document.createElement('img');
-        img.src = `https://images.pinkettu.com.ng/${src}`;
-        const button = document.createElement('button');
-        button.textContent = 'DELETE';
-        div.append(img, button);
-        return div;
-      }
-
-      const tiles = profile.images.slice(1).map(src => tile(src));
-      gallery.append(...tiles);
-      div.append(h3, gallery);
-      section.appendChild(div);
-      document.insertBefore(section, footer);
-    }
+    if (profile.images.length > 1) createGalleryForWorker();
   }
 }
 
--- js/signup.js
+++ js/signup.js
@@ -9,60 +9,66 @@ function validatePassword() {
   }
 }
 
-function sendImageToDatabase(file, filename) {
+async function sendImageToDatabase(file, filename) {
   const form = new FormData();
   form.append('File', file.files[0]);
   form.append('Filename', filename);
-  fetch('https://images.pinkettu.com.ng/upload.php', {
+  return fetch('https://images.pinkettu.com.ng/upload.php', {
     method: 'POST',
     body: form
-  }).then(res => console.log(res));
+  });
 }
 
 function handleSubmitResponse(request) {
   const { token } = JSON.parse(request.responseText);
-  if(!token) return;
-  
+  if (!token) return;
+
   localStorage.pinkettu = token;
   location.href = '/profile.html';
 }
 
-function toggleButtonSpinner(form) {
+function toggleButtonSpinner(form, isSubmitting) {
   const button = form.children.finalSubmit;
   [...button.children].forEach(child => child.classList.toggle('hide'));
+  localStorage.setItem('isSubmitting', isSubmitting);
 }
 
 function submitForm(e) {
   e.preventDefault();
-  toggleButtonSpinner(e.target);
+  if(localStorage.isSubmitting) return;
+
+  toggleButtonSpinner(e.target, true);
   const appIsLive = location.hostname !== '127.0.0.1';
   const API = appIsLive ? 'https://api.pinkettu.com.ng' : 'http://127.0.0.1:3001';
   const URL = `${API}/auth/signup`;
-  const [username, email, password, , worker, image] = e.target;
+  const [email, username, password, , worker, image] = e.target;
   const caption = Date.now().toString().slice(0, 10) + '.' + image.files[0].name;
-  sendImageToDatabase(image, caption);
+  const feedback = sendImageToDatabase(image, caption);
 
-  const xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function () {
-    if (this.readyState === 4 && this.status === 200) {
-      handleSubmitResponse(this);
-      toggleButtonSpinner(e.target);
-    }
-  };
-  xhttp.open("POST", URL, true);
-  xhttp.setRequestHeader("Content-type", "application/json");
-  xhttp.send(
-    JSON.stringify({
-      username: username.value,
-      email: email.value,
-      password: password.value,
-      worker: Boolean(worker.value),
-      image: caption
-    })
-  );
+  feedback.then(res => {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+      if (this.readyState === 4 && this.status === 200) {
+        handleSubmitResponse(this);
+        toggleButtonSpinner(e.target, false);
+      }
+    };
+    xhttp.open("POST", URL, true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send(
+      JSON.stringify({
+        username: username.value,
+        email: email.value,
+        password: password.value,
+        worker: worker.value === 'true',
+        image: caption
+      })
+    );
+  });
 }
 
 document.addEventListener('DOMContentLoaded', a => {
+  localStorage.removeItem('isSubmitting');
   const password = document.getElementById('pass');
   const confirmPassword = document.getElementById('confirm');
   const form = document.getElementsByTagName('form')[0];
