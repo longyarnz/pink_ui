763aedd82b666315eb4dc5799acb9d52238320c2
Olalekan Ayodele <longyarnz@gmail.com>
2019-03-17 21:16:28 +0100

    Complete account activation feature

--- js/activate.js
+++ js/activate.js
@@ -1,38 +1,59 @@
 const appIsLive = location.hostname !== '127.0.0.1';
 const API = appIsLive ? 'https://api.pinkettu.com.ng' : 'http://127.0.0.1:3001';
 const publicKey = 'pk_test_99aefb07d699525e9eed76be0cbe03fda6ad0ff6';
+const query = decodeURIComponent(location.search);
+const id = query.slice(6);
+let button;
 
-function handlePayment(response) {
+function handlePaymentResponse(response) {
+  response.id = id;
   const URL = `${API}/transaction/verify`;
   const xhttp = new XMLHttpRequest();
-  console.log(response);
 
   xhttp.onreadystatechange = function () {
     if (this.readyState === 4 && this.status === 200) {
       toggleButtonSpinner(button, false);
-      console.log(this.responseText);
+      const text = JSON.parse(this.responseText);
+      if (text === 'Activation is Verified') {
+        location.assign('/login.html');
+      }
+    }
+    else if (this.readyState === 4 && this.status === 400){
+      const p = document.querySelector('#signup-main > div > p');
+      p.textContent = 'Account Activation Failed';
+      p.style.color = '#d9534f';
+      p.style.fontWeight = '900';
     }
   };
-
+  
   xhttp.open("POST", URL, true);
+  xhttp.setRequestHeader('Content-type', 'application/json');
   xhttp.send(JSON.stringify(response));
 }
 
-function handleResponse(request) {
-  const { _id, user, message } = JSON.parse(request.responseText);
+function handleActivationResponse(request) {
+  const { id, user, message, status } = JSON.parse(request.responseText);
   localStorage.removeItem('isSubmitting');
-
+  
   if (message) location.assign('/login.html');
 
-  else if (_id && user) {
+  else if (status) {
+    const p = document.querySelector('#signup-main > div > p');
+    p.textContent = status;
+    p.style.color = '#5cb85c';
+    p.style.fontWeight = '900';
+    toggleButtonSpinner(button, false);
+  }
+  
+  else if (id && user) {
     const paystack = window.PaystackPop.setup({
       key: publicKey,
       email: user.email,
       amount: 100000,
-      ref: _id,
-      callback: handlePayment,
-      onClose: () => console.log("Payment Closed"),
-      currency: "NGN"
+      ref: id,
+      callback: handlePaymentResponse,
+      onClose: () => console.log('Payment Closed'),
+      currency: 'NGN'
     });
     paystack.openIframe();
   }
@@ -41,21 +62,14 @@ function handleResponse(request) {
 function activateViaPaystack() {
   if (localStorage.isSubmitting === 'true') return;
 
-  const button = document.querySelector('button');
   toggleButtonSpinner(button, true);
-
-  const query = decodeURIComponent(location.search);
-  const id = query.slice(6);
-
-  console.log('Activating');
 
   const URL = `${API}/transaction/activate/${id}`;
   const xhttp = new XMLHttpRequest();
 
   xhttp.onreadystatechange = function () {
     if (this.readyState === 4) {
-      toggleButtonSpinner(button, false);
-      handleResponse(this);
+      handleActivationResponse(this);
     }
   };
 
@@ -64,6 +78,6 @@ function activateViaPaystack() {
 }
 
 document.addEventListener('DOMContentLoaded', a => {
-  const button = document.querySelector('button');
+  button = document.querySelector('button');
   button.addEventListener('click', activateViaPaystack);
 });
