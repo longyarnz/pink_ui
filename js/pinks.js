4f094a08971053b8b34948e01f44c3daa4ef229c
Olalekan Ayodele <longyarnz@gmail.com>
2019-04-21 18:21:59 +0100

    Add SEO

---
+++ js/pinks.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', a => {
+    fetchPinks(profiles => {
+      if (Array.isArray(profiles) && profiles.length > 0) {
+        document.querySelectorAll('figure.placeholder-container').forEach(e => e.remove());
+        document.querySelector('div.see-more').classList.remove('hide');
+        profiles.forEach(
+          (i, o) => o < 6 && createPinkTiles(i._id, i.username, i.location, i.images.shift(), i.rank)
+        );
+      }
+    });
+  });
